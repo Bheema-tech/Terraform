5e8f899c5694f37e8d2e3cbaa75c2563dd149337
bheema-tech <bheemesha590@gmail.com>
2025-06-09 11:02:33 +0530

    Terraform-basics

---
+++ ec2.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allow_all_bheema_aws" {
+  name        = "Allow_all_bheema_aws"
+  description = "Allow all inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "aws-terraform-SG"
+  }
+}
